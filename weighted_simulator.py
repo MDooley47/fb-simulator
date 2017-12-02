838800d4fa7bc73c69e87416337eb5f9b6ff83f6
Matthew Dooley <Matthew@Dooley.xyz>
2017-12-01 21:36:47 -0600

    Increases chapter count
    Increases the chapter count from 1 to 3

--- weighted_simulator.py
+++ weighted_simulator.py
@@ -28,7 +28,7 @@ def gen_from_graph_api_json(file_name, state_size=2):
     (models, weights), models_weights = (map(list, zip(*models_weights)), None)
 
     markov_novel.Novel(
-        markovify.combine(models, weights), chapter_count=1
+        markovify.combine(models, weights), chapter_count=3
     ).write(novel_title=file_name, filetype='md')
 
 if __name__ == '__main__':
